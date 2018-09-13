824e2705eca9b516344dbd2d387c3fd3232f664b
MyAmbrose <43225850+MyAmbrose@users.noreply.github.com>
2018-09-13 21:47:18 +0800

    Add files via upload

---
+++ dahuilang.java
@@ -0,0 +1,22 @@
+  
+public class dahuilang
+{
+    public static void main(String[] args)
+  { 
+   
+
+
+        for( int i=99 ;i>0;i--)
+             {   
+                    System.out.println(i);
+                    System.out.println("chang tong yao...\n");
+         if(i==1)
+
+                    System.out.println("NO more bottles of beer on the wall");
+
+
+              }
+   } 
+}  
+
+                                                                               
