11dfb31fa2cefa32ef3dc87d5ebbad695f0deb43
MyAmbrose <43225850+MyAmbrose@users.noreply.github.com>
2018-09-14 21:56:17 +0800

    作业String c = "xyz";    String a;    a=c;a c地址

---
+++ string.java
@@ -0,0 +1,29 @@
+
+
+public class string 
+{
+	   public static void main(String[] args)
+	   {
+    String c = "xyz";
+    
+    String a;
+   
+    a=c;
+
+    a = "abcd";
+
+    System.out.println(a);
+    System.out.println(c);
+    System.out.println(a.hashCode());
+    System.out.println(c.hashCode());
+    
+	   }
+
+
+
+}    
+//Êä³ö£º
+abcd
+xyz
+2987074
+119193                                            
