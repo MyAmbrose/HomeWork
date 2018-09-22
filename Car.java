68c628702d904904c3afde7d8f1c2384281ac445
MyAmbrose <43225850+MyAmbrose@users.noreply.github.com>
2018-09-22 20:58:49 +0800

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,55 @@
+ import java.io.*;
+public class Car 
+{      
+	public static void gas()
+{
+System.out.println("加油：");
+int i;
+for(i=10;i<70;i=i+5)
+{
+	System.out.println(i);
+	if(i==65)
+	System.out.println("油箱已加满\n");
+}
+
+
+}
+public  static void run()
+{
+	int y=650;
+System.out.println("车子大约还能行驶");
+
+System.out.println(y+"公里");
+}
+ public static void main(String[] args)throws IOException
+	{ 
+     String name="Benchi";
+	 final  int tank=65;
+	 int oilConsumption=10;
+	  BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in)); 
+     
+      System.out.println("请正确输入你想知道的属性:"); 
+      String str = buffer.readLine();  
+      if (str=="name")
+      System.out.println(name); 
+      else if (str=="tank")
+          System.out.println(tank);   
+      else if (str=="oilConsumption")
+              System.out.println(oilConsumption); 
+      else 
+    	  System.out.println("您输入属性非法，请重新输入");
+      System.out.println(str);
+
+	System.out.println("车子品牌："+name);
+	System.out.println( "平均每升油大约行驶:"+oilConsumption+"公里");
+	System.out.println("油箱容量："+tank+"L");
+	gas();
+	run();
+	
+	}
+
+}
+
+
+
+
